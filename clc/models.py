058f80334b598bceab0790039d42193663ffaba6
user <user@moliere.(none)>
2011-02-27 17:09:37 +0100

    Translation system + french translation

--- clc/models.py
+++ clc/models.py
@@ -6,18 +6,31 @@ class ChallengeList(models.Model):
     name = models.CharField(max_length=200, unique=True)
     owner = models.OneToOneField(User, db_index=True, unique=True, related_name="challenge_list")
     deadline = models.DateTimeField(blank=True, null=True)
+    def __unicode__(self):
+        return self.name
 
 class Category(models.Model):
     name_en = models.CharField(max_length=200)
     name_fr = models.CharField(max_length=200)
+    name = property(
+        lambda self:getattr(self, "name_"+get_language(), self.name_en),
+        lambda self, value:setattr(self, "name_"+get_language(), value)
+    )
     color = models.CharField(max_length=10)
-    def get_name(self):
-        
-    name = property(get_name)
+    owner = models.ForeignKey(User, db_index=True, blank=True, null=True)
+    def __unicode__(self):
+        return self.name
 
 class Challenge(models.Model):
     challenge_list = models.ForeignKey(ChallengeList, db_index=True, related_name="challenges")
     description = models.CharField(max_length=200)
     category = models.ForeignKey(Category, db_index=True, blank=True, null=True)
     progress = models.PositiveSmallIntegerField(default=0)
-    
+    def __unicode__(self):
+        return repr((
+            self.id,
+            self.challenge_list,
+            self.description,
+            self.category,
+            self.progress,
+        ))
