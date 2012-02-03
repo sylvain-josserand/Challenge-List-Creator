29c79f32c319bf9e85f8de374c71fcb1e6e4486f
joss82 <joss82@gmail.com>
2012-02-03 21:15:01 +0700

    Add: Reset password mechanism Add: Generic message page Fix: Logout from index page

--- clc/views.py
+++ clc/views.py
@@ -11,6 +11,7 @@ from django.forms.util import ErrorList
 
 import smtplib
 from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
 
 import datetime
 from os import getenv
@@ -34,16 +35,19 @@ class ChallengeForm(forms.ModelForm):
     due_date = forms.DateField()
     hidden = forms.CharField(widget=forms.HiddenInput, initial="challenge")
 
+def message_render(request, message):
+    return render_to_response(
+        'message.html', {'message':_(message)},  context_instance=RequestContext(request)
+    )
+
 def display(request, list_name):
     list_name = list_name.lower()
     try:
         cl = ChallengeList.objects.get(name=list_name)
     except ChallengeList.DoesNotExist:
-        return render_to_response('base.html', {'content':_("Sorry but this challenge list does not exist. Want to create it?<a href='/'>Click here.</a>")})
+        return render_to_response('message.html', {'message':_("Sorry but this challenge list does not exist. Want to create it?<a href='/'>Click here.</a>")}, context_instance=RequestContext(request))
         
-    from django.db import connection
     challenge_instances = ChallengeInstance.objects.filter(challenge_list=cl).order_by("challenge__category")
-    queries = connection.queries
     # Is user logged in?
     logged_in = request.user.is_authenticated()
     
@@ -72,6 +76,123 @@ def display(request, list_name):
         'display.html', locals(), context_instance=RequestContext(request)
     )
 
+
+def reset_password(request, list_name):
+    try:
+        user = auth.models.User.objects.get(username=list_name)
+    except auth.models.User.DoesNotExist:
+        return message_render(request, _("Sorry, but this user does not exist."))
+    authkey = request.GET.get('authkey', None)
+    if not authkey:
+        return message_render(request, _("Sorry, but the authentication key is missing."))
+    if authkey != user.password.split('$')[-1]:
+        return message_render(request, _("Sorry, but the authentication key is wrong."))
+    if request.method == 'GET':
+        signup_form = SignupForm({'username':user.username, 'email':user.email, 'hidden':'signup'})
+        signup_form._errors = {}
+    elif request.method == 'POST':
+        from sys import stderr
+        stderr.write("POST OK\n")
+        signup_form = SignupForm(request.POST)
+        stderr.write("signup_form\n")
+        if signup_form.is_valid():
+            password = request.POST['password']
+            password2 = request.POST['password2']
+            if password == password2:
+                stderr.write("passwords match\n")
+                user.set_password(password)
+                user.save()
+                user = auth.authenticate(username=user.username, password=password)
+                if user is not None:
+                    stderr.write("user is not None\n")
+                    if user.is_active:
+                        stderr.write("user is active\n")
+                        auth.login(request, user)
+                        stderr.write("HttpResponseRedirect(user.username)\n")
+                        return HttpResponseRedirect('/'+user.username)
+                    else:
+                        return message_render(request, _("Sorry, but this account is disabled."))
+                else:
+                    return message_render(request, _("Sorry, but this account is invalid."))
+            else:
+                signup_form._errors['password'] = signup_form._errors['password2'] = ErrorList((_('passwords mismatch'),)) 
+        else:
+            stderr.write("Invalid signup_form\n")
+            stderr.write(str(signup_form._errors))
+            stderr.write('\n')
+    return render_to_response('reset_password.html', locals(), context_instance=RequestContext(request))
+
+
+def send_password(request):
+    if request.method != 'POST':
+        return HttpResponse(__("You should try a POST request"))
+    if "email" in request.POST:
+        email = request.POST["email"]
+        text_template = _("""\
+Hi %(username)s,
+
+You couldn't remember your password. That's ok.
+
+Please click the link below to set a new one:
+http://www.challengelistcreator.com/reset_password/%(username)s?authkey=%(authkey)s
+
+Have fun,
+
+The webmaster at challenge list creator.
+""")
+
+        html_template = _("""\
+<html><body>
+<a href=http://www.challengelistcreator.com style="border:0;" ><img src="http://www.challengelistcreator.com/media/images/logo.png" /></a><br/>
+<br/>
+Hi %(username)s,<br/>
+<br/>
+You couldn't remember your password. That's ok.<br/>
+<br/>
+<b>Please click the link below to set a new one:</b><br/>
+<a href='http://www.challengelistcreator.com/reset_password/%(username)s?authkey=%(authkey)s'>Reset your password</a><br/>
+<br/>
+Have fun,<br/>
+<br/>
+The webmaster at challenge list creator.<br/>
+<br/>
+</body></html>
+""")
+        admin_email = getenv('ADMIN_EMAIL')
+        try:
+            user = auth.models.User.objects.get(email=email)
+        except auth.models.User.DoesNotExist:
+            return HttpResponse(False)
+        d = {'username':user.username, 'authkey':user.password.split('$')[-1]}
+        text = text_template % d
+        html = html_template % d
+        msg = MIMEMultipart('alternative')
+        part1 = MIMEText(text.encode("utf-8"), "plain", "utf-8")
+        part2 = MIMEText(html.encode("utf-8"), "html", "utf-8")
+        msg.attach(part1)
+        msg.attach(part2)
+        msg['From'] = admin_email
+        msg['To'] = email
+        msg['Subject'] = _("You have asked to reset your password")
+        smtp = smtplib.SMTP(getenv('SMTP_SERVER'))
+        try:
+            smtp.login(getenv('SMTP_USERNAME'), getenv('SMTP_PASSWORD'))
+            smtp.sendmail(
+                admin_email,
+                email,
+                msg.as_string()
+            )
+        except:
+            return HttpResponse(False)
+        else:
+            return HttpResponse(True)
+
+        finally:
+            if smtp:
+                smtp.quit()
+    else:
+        return HttpResponse(False)
+
 @login_required
 def delete(request):
     if request.method != 'POST':
@@ -88,6 +209,8 @@ def delete(request):
         friend = my_cl.friends.get(owner=id)
         my_cl.friends.remove(friend)
         return HttpResponse(id)
+    else:
+        return HttpResponse(0)
 
 @login_required
 def save(request):
@@ -181,6 +304,7 @@ def add(request):
 
 def index(request):
     signup_form = login_form = None
+    logged_in = request.user.is_authenticated()
     if request.method == 'POST':
         valid_form = False
         if request.POST['hidden'] == "login":
@@ -202,15 +326,19 @@ def index(request):
             if valid_form:
                 username = request.POST['username'].lower()
                 email = request.POST['email']
-                try:
-                    user = auth.models.User.objects.create_user(username, email, password)
-                except IntegrityError:
-                    user = auth.authenticate(username=username, password=password)
-                    if not user:
-                        signup_form._errors['username'] = ErrorList((_("A user with the same name already exists. But the password you entered did not match."),))
-                        valid_form = False
+                if len(auth.models.User.objects.filter(email=email)) == 0:
+                    try:
+                        user = auth.models.User.objects.create_user(username, email, password)
+                    except IntegrityError:
+                        user = auth.authenticate(username=username, password=password)
+                        if not user:
+                            signup_form._errors['username'] = ErrorList((_("A user with the same name already exists. But the password you entered did not match."),))
+                            valid_form = False
+                    else:
+                        user = auth.authenticate(username=username, password=password)
                 else:
-                    user = auth.authenticate(username=username, password=password)
+                    signup_form._errors['email'] = ErrorList((_("Email already exists."),))
+                    valid_form = False
                 if valid_form:
                     try:
                         cl = user.challenge_list
